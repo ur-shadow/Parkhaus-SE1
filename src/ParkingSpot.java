bc17603b49edf576042abbdd9a1e849b3019179e
Twizzle <the3.14159268483279@gmail.com>
2022-04-12 19:14:30 +0200

    fix formatting

--- src/ParkingSpot.java
+++ src/ParkingSpot.java
@@ -2,27 +2,32 @@ import java.util.UUID;
 
 public class ParkingSpot implements IParkingSpot {
     @Override
-    public void markSpot(int PNr){
+    public void markSpot(int PNr) {
 
     }
+
     @Override
-    public void reserveSpot(int PNr){
+    public void reserveSpot(int PNr) {
 
     }
+
     @Override
-    public int getNextSpot(){
+    public int getNextSpot() {
         return 0;
     }
+
     @Override
-    public int getNextHSpot(){
+    public int getNextHSpot() {
         return 0;
     }
+
     @Override
-    public UUID createTicket(int PNr){
+    public UUID createTicket(int PNr) {
         return UUID.randomUUID();
     }
+
     @Override
-    public Boolean[] getEmptySpots(){
+    public Boolean[] getEmptySpots() {
         Boolean[] Spots = new Boolean[]{};
         return Spots;
     }
